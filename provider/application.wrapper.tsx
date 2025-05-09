eed92769aeacbcd60a5243087d960fd93bb5f253
Naresh Bhosale <nareshbhosale73@gmail.com>
2025-05-09 18:01:28 +0530

    Integrate React Suspense for loading state in ApplicationClientWrapper. Wrapped NuqsAdapter in Suspense to display a loading indicator while children components are being loaded, enhancing user experience during data fetching.

--- provider/application.wrapper.tsx
+++ provider/application.wrapper.tsx
@@ -2,7 +2,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { NuqsAdapter } from "nuqs/adapters/next";
-import React from "react";
+import React, { Suspense } from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -21,8 +21,15 @@ const ApplicationClientWrapper = ({ children }: Props) => {
   return (
     <SessionProvider refetchInterval={0} refetchOnWindowFocus={false}>
       <QueryClientProvider client={queryClient}>
-        {" "}
-        <NuqsAdapter>{children}</NuqsAdapter>
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center h-screen">
+              Loading...
+            </div>
+          }
+        >
+          <NuqsAdapter>{children}</NuqsAdapter>
+        </Suspense>
       </QueryClientProvider>
     </SessionProvider>
   );
